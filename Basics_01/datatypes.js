e86075e773c09c4d320a8d1bc686b393d59785c0
Mohak Gupta <62827229+MohakGupta2004@users.noreply.github.com>
2024-01-23 20:30:09 +0000

    new datatypes added

--- Basics_01/datatypes.js
+++ Basics_01/datatypes.js
@@ -13,7 +13,8 @@ let name="Mohak"
 let isLoggedIn=true
 let valueUndefined;
 let valueNull=null
-
+let symbol=Symbol('123')
+let symbol2=Symbol('123')
 /* 
     There are number of datatypes in javascript
         1. number => number types
@@ -22,13 +23,29 @@ let valueNull=null
         4. undefined => which is not defined yet
         5. null=> simply means empty (ex: Imagine a weather app, if it didn't find any weather of a place
                                           So what will it use 0 or nothing. Yes that nothing is null)
-        6. symbol => defining symbol, mostly used in react
+        6. symbol => defining symbol, it creates unique values 
         7. object => a non primitive datatype
 
 */
 console.log(typeof(valueUndefined)) //output: undefined
 console.log(typeof(valueNull))//output: object
 
-
+console.log(symbol==symbol2) //output false because both have different ID
 //For more knowledge use documentation: MDN references, tc39(offcial ecmascript site[Ecma script is
 //                                                         the organisation which defined standards for js])
+
+
+
+const array= ["This","is","an","Array"]
+const myobj= {
+    name: "This Object",
+    ID: 123,
+    description: "Although it looks like JSON but it's not"
+}
+
+function helloWorld() {
+    console.log("This is a function")
+    console.log("Hello World")
+}
+
+helloWorld()
