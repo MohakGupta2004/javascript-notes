bdb21c796b3428e18187a11ef24373d4569a48a5
Mohak Gupta <62827229+MohakGupta2004@users.noreply.github.com>
2024-01-24 15:12:57 +0000

    math and number

---
+++ Basics_01/Maths_and_Number.js
@@ -0,0 +1,12 @@
+
+//Number and it's functio and uses
+const number=new Number(131.4467)
+console.log(number.toString())
+console.log(number.toPrecision(4))
+
+
+//Maths 
+console.log(Math.floor((Math.random()*10)+1)) // generate number between 0-10
+min=10
+max=20
+console.log(Math.floor((Math.random()*(max-min+1))+min))//generate number between 10-20
