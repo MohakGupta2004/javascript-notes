ca59073bc3a3645f681aa7f6c3c6a6543e88f75c
MohakGupta500 <mohakgupta500@gmail.com>
2024-02-09 20:55:09 -0500

    Json, destructurization and API's definition

---
+++ Basics_02/JSONAPI.js
@@ -0,0 +1,29 @@
+const course = {
+    courseInstructor: "JS cheat sheet",
+    email: "mohak@gmail.com",
+    value: 1
+}
+
+//We can destructure the Object
+const {courseInstructor: name} = course
+console.log(name)
+
+
+/*JSON: JSON is an open standard file format and data interchange format that uses human-readable text to store and transmit data objects consisting of attribute–value pairs and arrays.
+ex: 
+{
+    "name": "Mohak",
+    "email": "Mohak@gmail.com",
+    "ROll": 23
+}
+
+
+API: An application programming interface is a way for two or more computer programs or components to communicate with each other.
+Api usually comes in json or array format
+
+ex: 
+[
+    {},
+    {},
+]
+*/
